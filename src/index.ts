7800a559e944524553a73614220efe6f76ca462e
Ryo Ota <nwtgck@gmail.com>
2019-06-11 11:36:09 +0900

    [add/remove] Implement pipingDeploy() and remove main.rb

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as url from 'url';
 import * as path from 'path';
 import * as fs from 'fs';
 import thenRequest, {ResponsePromise} from 'then-request';
+import * as glob from 'glob';
 
 const REALOAD_WAIT_SEC = 10;
 
@@ -24,22 +25,6 @@ function pipingSend(serverUrl: string, deployId: string, filePath: string, body:
     body: body,
   });
 }
-
-// def piping_send(server_url, deploy_id, file_path, body_f)
-//   url = File.join(server_url, deploy_id, file_path)
-//   puts("URL: #{url}")
-//   uri = URI(url)
-//   req = Net::HTTP::Post.new(uri.path)
-//   req.body_stream = body_f
-//   req.content_length = body_f.size
-//   http = Net::HTTP.new(uri.host, uri.port)
-//   if uri.scheme == "https"
-//     http.use_ssl = true
-//     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-//   end
-//   res = http.request(req)
-//   res
-// end
 
 async function deployLoop(serverUrl: string, deployId: string, bodyFilePath: string, reqFilePath: string): Promise<void> {
   if (!fs.statSync(bodyFilePath).isFile()) {
@@ -62,72 +47,36 @@ async function deployLoop(serverUrl: string, deployId: string, bodyFilePath: str
   }
 }
 
-// def deploy_loop_thread(server_url, deploy_id, body_file_path, req_file_path)
-//   if !File.file?(body_file_path)
-//     return
-//   end
-//   Thread.start{
-//     loop {
-//       puts("Loading #{body_file_path} as #{req_file_path}...")
-//       begin
-//         open(body_file_path){|body_f|
-//           res = piping_send(
-//             server_url,
-//             deploy_id,
-//             req_file_path,
-//             body_f
-//           )
-//           if res.code == "200"
-//             puts("send status: #{res.code}")
-//           else
-//             sleep REALOAD_WAIT_SEC
-//           end
-//         }
-//       rescue
-//         p $!
-//         sleep REALOAD_WAIT_SEC
-//       end
-//     }
-//   }
-// end
+function pipingDeploy(serverUrl: string, deployId: string, publicDirPath: string, indexPath?: string): void {
+  if (indexPath !== undefined) {
+    deployLoop(serverUrl, deployId, indexPath, "");
+    deployLoop(serverUrl, deployId, indexPath, "/");
+  }
 
-// def piping_deploy(server_url, deploy_id, public_dir_path, index_path=nil)
-//   threads = []
-//   if !index_path.nil?
-//     # Enroll index
-//     threads << deploy_loop_thread(server_url, deploy_id, index_path, "")
-//     threads << deploy_loop_thread(server_url, deploy_id, index_path, "/")
-//   end
-//   fpaths = Dir.chdir(public_dir_path){
-//     Dir.glob("**/*")
-//   }
-//   fpaths.each{|file_path|
-//       joined_fpath = File.join(public_dir_path, file_path)
-//       threads << deploy_loop_thread(
-//         server_url,
-//         deploy_id,
-//         joined_fpath,
-//         file_path
-//       )
-//     }
-//   threads.each(&:join)
-// end
-
+  // Backup pwd
+  const backupCwd = process.cwd();
+  // Move to public directory path
+  process.chdir(publicDirPath);
+  // Get file paths in the directory
+  glob('**/*', (err, filePaths) => {
+    if(err !== null) {
+      console.error(err);
+      return;
+    }
+    // Move pwd back
+    process.chdir(backupCwd);
+    for (const filePath of filePaths) {
+      // Join the path
+      const joinedFPath = path.join(publicDirPath, filePath);
+      // Deploy
+      deployLoop(serverUrl, deployId, joinedFPath, filePath);
+    }
+  });
+}
 
 
 const serverUrl      = "https://ppng.ml";
 const deployId       = "mydeployid";
 
 
-deployLoop(serverUrl, deployId, './README.md', '/');
-
-// // const public_dir_path = "public_html"
-// index_path      = "public_html/index.html" # NOTE: This can be nil
-//
-// piping_deploy(
-//   server_url,
-//   deploy_id,
-//   public_dir_path,
-//   index_path
-// )
-
+pipingDeploy(serverUrl, deployId, './dist', 'dist/test/index.test.js');
