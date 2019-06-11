7b36cac962b98b095f5cfd401e0e2ebe4b67482f
Ryo Ota <nwtgck@gmail.com>
2019-06-11 11:58:31 +0900

    [change] Rename "deploy" to "host"

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,14 @@ import * as fs from 'fs';
 import * as request from 'request';
 import * as glob from 'glob';
 
-const REALOAD_WAIT_SEC = 10;
+const REHOST_WAIT_SEC = 10;
 
 function sleep(millis: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, millis));
 }
 
-function pipingSend(serverUrl: string, deployId: string, filePath: string, body: stream.Readable): Promise<request.Response> {
-  const u = url.resolve(serverUrl, path.join(deployId, filePath));
+function pipingSend(serverUrl: string, hostId: string, filePath: string, body: stream.Readable): Promise<request.Response> {
+  const u = url.resolve(serverUrl, path.join(hostId, filePath));
   console.log(`URL: ${u}`);
 
   return new Promise((resolve, reject) => {
@@ -31,7 +31,7 @@ function pipingSend(serverUrl: string, deployId: string, filePath: string, body:
   });
 }
 
-async function deployLoop(serverUrl: string, deployId: string, bodyFilePath: string, reqFilePath: string): Promise<void> {
+async function hostLoop(serverUrl: string, hostId: string, bodyFilePath: string, reqFilePath: string): Promise<void> {
   if (!fs.statSync(bodyFilePath).isFile()) {
     return;
   }
@@ -39,23 +39,24 @@ async function deployLoop(serverUrl: string, deployId: string, bodyFilePath: str
     console.log(`Loading ${bodyFilePath} as ${reqFilePath}...`);
     try {
       const body = fs.createReadStream(bodyFilePath);
-      const res = await pipingSend(serverUrl, deployId, reqFilePath, body);
+      const res = await pipingSend(serverUrl, hostId, reqFilePath, body);
       if (res.statusCode === 200) {
         console.log(`send status: ${res.statusCode}`);
       } else {
-        await sleep(REALOAD_WAIT_SEC * 1000);
+        await sleep(REHOST_WAIT_SEC * 1000);
       }
     } catch (err) {
       console.error(err);
-      await sleep(REALOAD_WAIT_SEC * 1000);
+      await sleep(REHOST_WAIT_SEC * 1000);
     }
   }
 }
 
-function pipingDeploy(serverUrl: string, deployId: string, publicDirPath: string, indexPath?: string): void {
+function host(serverUrl: string, hostId: string, publicDirPath: string, indexPath?: string): void {
   if (indexPath !== undefined) {
-    deployLoop(serverUrl, deployId, indexPath, "");
-    deployLoop(serverUrl, deployId, indexPath, "/");
+    // Host index paths
+    hostLoop(serverUrl, hostId, indexPath, "");
+    hostLoop(serverUrl, hostId, indexPath, "/");
   }
 
   // Backup pwd
@@ -73,15 +74,14 @@ function pipingDeploy(serverUrl: string, deployId: string, publicDirPath: string
     for (const filePath of filePaths) {
       // Join the path
       const joinedFPath = path.join(publicDirPath, filePath);
-      // Deploy
-      deployLoop(serverUrl, deployId, joinedFPath, filePath);
+      // Host
+      hostLoop(serverUrl, hostId, joinedFPath, filePath);
     }
   });
 }
 
 
 const serverUrl      = "https://ppng.ml";
-const deployId       = "mydeployid";
+const hostId       = "myhostId";
 
-
-pipingDeploy(serverUrl, deployId, './dist', 'dist/test/index.test.js');
+host(serverUrl, hostId, './dist', 'dist/test/index.test.js');
