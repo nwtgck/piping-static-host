21d91e3f6dcd34bbe4777183482f306c7f74269d
Ryo Ota <nwtgck@nwtgck.org>
2021-02-14 09:40:36 +0900

    ppng.ml → ppng.io

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const parser = yargs
   .option('server-url', {
     describe: "Piping Server URL",
     type: 'string',
-    default: 'https://ppng.ml'
+    default: 'https://ppng.io'
   })
   .option('host-prefix', {
     describe: "Host prefix (default: random)",
